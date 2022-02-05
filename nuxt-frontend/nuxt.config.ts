eedaba715a6fd3d35d481439d1582f081a6595bb
Кирилл <k.ilin@qsoft.ru>
2022-02-05 15:31:01 +0300

    front / tailwind add

--- nuxt-frontend/nuxt.config.ts
+++ nuxt-frontend/nuxt.config.ts
@@ -1,6 +1,24 @@
 import { defineNuxtConfig } from 'nuxt3'
+import { resolve } from 'pathe'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    modules: [
+        // Using package name
+        // '@nuxtjs/axios',
+    ],
+    alias: {
+        '@': resolve(__dirname),
+        '~': resolve(__dirname),
+    },
+    css: ['~/assets/css/tailwind.css'],
+    build: {
+        postcss: {
+            // add Postcss options
+            postcssOptions: require('./postcss.config.js'),
+        },
+    },
+    // Restart server when these change
+    watch: ['~/postcss.config.js', '~/tailwind.config.js'],
+        // Axios options here
 })
